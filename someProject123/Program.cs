14db4ae1352b24a5464f3cb7d2f7671f9194e9ba
Ilya Maltsev <145224635+noble-comrade@users.noreply.github.com>
2024-10-28 16:19:01 +0300

    Add files via upload

---
+++ someProject123/Program.cs
@@ -0,0 +1,16 @@
+﻿string pathToRead = "C:\\Users\\prdb\\Desktop\\someProject123\\file.txt";
+string pathToWrite = "C:\\Users\\prdb\\Desktop\\someProject123\\answer.txt";
+int result = 0;
+
+StreamReader reader = new StreamReader(pathToRead);
+StreamWriter writer = new StreamWriter(pathToWrite);
+
+string[] phrase = reader.ReadLine().Split(" ");
+
+if (phrase[1] == "+") result = Convert.ToInt32(phrase[0]) + Convert.ToInt32(phrase[2]);
+if (phrase[1] == "-") result = Convert.ToInt32(phrase[0]) - Convert.ToInt32(phrase[2]);
+if (phrase[1] == "*") result = Convert.ToInt32(phrase[0]) * Convert.ToInt32(phrase[2]);
+if (phrase[1] == "/") result = Convert.ToInt32(phrase[0]) / Convert.ToInt32(phrase[2]);
+
+writer.WriteLine(Convert.ToString(result));
+Console.WriteLine(result);
